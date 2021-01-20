c5a834f1fd852dd63f5727932807f4ad941235b2
Joe Shannon <jpshanno@mtu.edu>
2021-01-19 22:46:56 -0500

    Figured out time series models

--- code/time_series_models.R
+++ code/time_series_models.R
@@ -12,80 +12,228 @@ tar_load(water_budget)
 dat <- 
   water_budget[site == "152" & sample_year == 2017]
 
-# Prewhitening example to look at ccf of pet and wl_initial:
-# https://online.stat.psu.edu/stat510/lesson/9/9.1
-# pet has an ARIMA structure:
-forecast::auto.arima(dat[!is.na(wl_initial_cm), pet_cm])
+# # Prewhitening example to look at ccf of pet and wl_initial:
+# # https://online.stat.psu.edu/stat510/lesson/9/9.1
+# # pet has an ARIMA structure:
+# forecast::auto.arima(dat[!is.na(wl_initial_cm), pet_cm])
+# 
+# prewhite_wl <- 
+#   filter(x = dat[!is.na(wl_initial_cm), wl_initial_cm],
+#          filter = c(coef(forecast::auto.arima(dat[!is.na(wl_initial_cm), pet_cm]))),
+#          method = "con",
+#          sides = 1)
+# 
+# ccf(x = resid(forecast::auto.arima(dat[!is.na(wl_initial_cm), pet_cm])), 
+#     y = prewhite_wl,
+#     na.action = na.pass)
+# 
+# # precip has an ARIMA structure
+# forecast::auto.arima(dat[!is.na(wl_initial_cm), best_precip_cm], allowmean = FALSE)
+# 
+# # But so we can prewhiten the prewhiteded wl_initial using the ARIMA structure for precip
+# pre_prewhite_wl <- 
+#   filter(prewhite_wl,
+#          filter = coef(forecast::auto.arima(dat[!is.na(wl_initial_cm), best_precip_cm], allowmean = FALSE)),
+#          method = "con",
+#          sides = 1)
+# 
+# ccf(x = dat[!is.na(wl_initial_cm), best_precip_cm], 
+#     y = filter(prewhite_wl,
+#                filter = coef(forecast::auto.arima(dat[!is.na(wl_initial_cm), best_precip_cm], allowmean = FALSE)),
+#                method = "con",
+#                sides = 1),
+#     na.action = na.pass)
+# 
+# # My final models predict Ds which shows now ARIMA structure, because it is the
+# # differenced water level and water level has an ARIMA(0,1,0) structure after the
+# # effects of PET and precip are accounted for
+# auto.arima()
+# 
+# ccf(x = resid(forecast::auto.arima(dat[!is.na(wl_initial_cm), pet_cm])), 
+#     y = filter(x = dat[!is.na(wl_initial_cm), wl_initial_cm],
+#                filter = c(coef(forecast::auto.arima(dat[!is.na(wl_initial_cm), pet_cm]))),
+#                method = "con",
+#                sides = 1),
+#     na.action = na.pass)
+# 
+#   
+# dat[, c("prev_precip", "prev2_precip", "prev3_precip", "prev4_precip") := map(1:4, ~shift(best_precip_cm, .x))]
+# dat[, c("prev_pet", "prev2_pet", "prev3_pet", "prev4_pet") := map(1:4, ~shift(pet_cm, .x))]
+# dat[, c("diff_precip", "diff2_precip", "diff3_precip", "diff4_precip",
+#         "diff_pet", "diff2_pet", "diff3_pet", "diff4_pet") := map(.SD, ~c(0, diff(.x))),
+#     .SDcols = c("prev_precip", "prev2_precip", "prev3_precip", "prev4_precip",
+#                 "prev_pet", "prev2_pet", "prev3_pet", "prev4_pet")]
+# dat[, prev_wl := shift(wl_initial_cm, 1)]
+# dat[, prev2_wl := shift(wl_initial_cm, 2)]
+# dat[, prev_ds := shift(Ds_cm, 1)]
+# dat[, prev2_ds := shift(Ds_cm, 2)]
+# 
+# big_lm <- 
+#   lm(wl_initial_cm ~ 0 + offset(prev_wl) + best_precip_cm + prev_precip + prev2_precip + prev3_precip + pet_cm + prev_pet, data = dat)
+# 
+# dat[, wl_lm := predict(big_lm, newdata = .SD)]
+# 
+# 
+# plot(wl_lm ~ doy, data = dat[!is.na(wl_initial_cm)], type = 'l')
+# lines(wl_initial_cm ~ doy, data = dat[!is.na(wl_initial_cm)], col = "blue")
+# 
+# ccf(dat[!is.na(wl_initial_cm), wl_initial_cm],
+#     dat[!is.na(wl_initial_cm), best_precip_cm])
+# 
+# ccf(dat[!is.na(wl_initial_cm), wl_initial_cm],
+#     dat[!is.na(wl_initial_cm), pet_cm])
+# 
+# 
+# 
+# 
+# 
+# dat[, c("prev_precip", "prev2_precip", "prev3_precip", "prev4_precip") := map(1:4, ~shift(best_precip_cm, .x))]
+# dat[, c("prev_pet", "prev2_pet", "prev3_pet", "prev4_pet") := map(1:4, ~shift(pet_cm, .x))]
+# dat[, c("diff_precip", "diff2_precip", "diff3_precip", "diff4_precip",
+#         "diff_pet", "diff2_pet", "diff3_pet", "diff4_pet") := map(.SD, ~c(0, diff(.x))),
+#     .SDcols = c("prev_precip", "prev2_precip", "prev3_precip", "prev4_precip",
+#                 "prev_pet", "prev2_pet", "prev3_pet", "prev4_pet")]
+# dat[, prev_wl := shift(wl_initial_cm, 1)]
+# dat[, prev2_wl := shift(wl_initial_cm, 2)]
+# dat[, prev_ds := shift(Ds_cm, 1)]
+# dat[, prev2_ds := shift(Ds_cm, 2)]
 
-prewhite_wl <- 
-  filter(x = dat[!is.na(wl_initial_cm), wl_initial_cm],
-         filter = c(coef(forecast::auto.arima(dat[!is.na(wl_initial_cm), pet_cm]))),
-         method = "con",
-         sides = 1)
+dat <- 
+  tar_read(water_budget)[site == "152" & sample_year == 2017]
 
-ccf(x = resid(forecast::auto.arima(dat[!is.na(wl_initial_cm), pet_cm])), 
-    y = prewhite_wl,
-    na.action = na.pass)
+start_doy <- 
+  dat[!is.na(wl_initial_cm), doy][1]
 
-# precip has an ARIMA structure
-forecast::auto.arima(dat[!is.na(wl_initial_cm), best_precip_cm], allowmean = FALSE)
+WL <- 
+  dat[!is.na(wl_initial_cm), wl_initial_cm]
+L1WL <- 
+  dat[, .(wl_initial_cm = shift(wl_initial_cm, 1))][!is.na(wl_initial_cm), c(wl_initial_cm, last(wl_initial_cm))]
+DS <- 
+  dat[!is.na(wl_initial_cm), Ds_cm]
+P <- 
+  dat[!is.na(wl_initial_cm), best_precip_cm]
+PET <- 
+  dat[!is.na(wl_initial_cm), pet_cm]
+L1P <- 
+  dat[, .(wl_initial_cm, p = shift(best_precip_cm, 1))][!is.na(wl_initial_cm), p]
+L2P <- 
+  dat[, .(wl_initial_cm, p = shift(best_precip_cm, 2))][!is.na(wl_initial_cm), p]
+L3P <- 
+  dat[, .(wl_initial_cm, p = shift(best_precip_cm, 3))][!is.na(wl_initial_cm), p]
+L4P <- 
+  dat[, .(wl_initial_cm, p = shift(best_precip_cm, 4))][!is.na(wl_initial_cm), p]
+L1PET <- 
+  dat[, .(wl_initial_cm, p = shift(pet_cm, 1))][!is.na(wl_initial_cm), p]
+L2PET <- 
+  dat[, .(wl_initial_cm, p = shift(pet_cm, 2))][!is.na(wl_initial_cm), p]
+L3PET <- 
+  dat[, .(wl_initial_cm, p = shift(pet_cm, 3))][!is.na(wl_initial_cm), p]
+L4PET <- 
+  dat[, .(wl_initial_cm, p = shift(pet_cm, 4))][!is.na(wl_initial_cm), p]
 
-# But so we can prewhiten the prewhiteded wl_initial using the ARIMA structure for precip
-pre_prewhite_wl <- 
-  filter(prewhite_wl,
-         filter = coef(forecast::auto.arima(dat[!is.na(wl_initial_cm), best_precip_cm], allowmean = FALSE)),
-         method = "con",
-         sides = 1)
+# Probably should do just precip, filter out the effects and then look at PET
+# Having trouble implementing that right now (this model does not filter out 
+# all of the effects of precip). That may make it possilbe to relate actual 
+# drawdown rate to PET. Right now it's a fixed drawdown across the growing 
+# season. This may be in part because of underlying relationships between WL,
+# PET, SY, and Q also creating additional noise
+# Looks like 2012 is not performing great because of the very dry conditions and
+# a fixed draw down rate as described above
+mod <- 
+  arimax(WL, order = c(1, 1, 0), 
+         xtransf = matrix(c(L1P, L1PET), ncol = 2), 
+         transfer = list(c(2,0), c(0,0)))
 
-ccf(x = dat[!is.na(wl_initial_cm), best_precip_cm], 
-    y = filter(prewhite_wl,
-               filter = coef(forecast::auto.arima(dat[!is.na(wl_initial_cm), best_precip_cm], allowmean = FALSE)),
-               method = "con",
-               sides = 1),
-    na.action = na.pass)
+coefs <- 
+  coef(mod)
 
-# My final models predict Ds which shows now ARIMA structure, because it is the
-# differenced water level and water level has an ARIMA(0,1,0) structure after the
-# effects of PET and precip are accounted for
-auto.arima()
+p_phi1 <- 
+  coefs[["T1-AR1"]]
 
-ccf(x = resid(forecast::auto.arima(dat[!is.na(wl_initial_cm), pet_cm])), 
-    y = filter(x = dat[!is.na(wl_initial_cm), wl_initial_cm],
-               filter = c(coef(forecast::auto.arima(dat[!is.na(wl_initial_cm), pet_cm]))),
-               method = "con",
-               sides = 1),
-    na.action = na.pass)
+p_phi2 <- 
+  coefs[["T1-AR2"]]
 
+p_theta0 <- 
+  coefs[["T1-MA0"]]
+
+pet_theta0 <- 
+  coefs[["T2-MA0"]]
+
+lmrob(fitted(forecast::Arima(WL,
+                             model = forecast::Arima(p_theta0 * L1P, order = c(2, 0, 0), 
+                                                     fixed = c(p_phi1, p_phi2, 0)))) ~ PET)
+
+HAT <- 
+  WL[1] +
+  cumsum((p_theta0 * filter(L1P, c(p_phi1, p_phi2), "con", sides = 1) +
+        pet_theta0 * L1PET)[-c(1:2)])
+# WL[1] +
+#   cumsum((
+#     4.3834 * (L1P - mean(L1P)) +
+#       -2.2646 * filter((L1P - mean(L1P)), c(1.2233, -0.2596), "con", sides = 1) + 
+#       -0.7217 * (L1PET - mean(L1PET))
+#   )[-c(1:2)])
+
+plot(HAT, 
+     type = 'l',
+     ylim = c(min(c(HAT, WL), na.rm = TRUE), 
+              max(c(HAT, WL), na.rm = TRUE)))
+lines(WL, col = "blue")
+
+# morpho_mods <- 
+#   tar_read(mod_morphology_flow)
+# 
+# flow_mod <- 
+#   morpho_mods[site == "152", f_predict[[1]]]
+# 
+# cp <- 
+#   morpho_mods[site == "152", changepoint]
   
-dat[, c("prev_precip", "prev2_precip", "prev3_precip", "prev4_precip") := map(1:4, ~shift(best_precip_cm, .x))]
-dat[, c("prev_pet", "prev2_pet", "prev3_pet", "prev4_pet") := map(1:4, ~shift(pet_cm, .x))]
-dat[, c("diff_precip", "diff2_precip", "diff3_precip", "diff4_precip",
-        "diff_pet", "diff2_pet", "diff3_pet", "diff4_pet") := map(.SD, ~c(0, diff(.x))),
-    .SDcols = c("prev_precip", "prev2_precip", "prev3_precip", "prev4_precip",
-                "prev_pet", "prev2_pet", "prev3_pet", "prev4_pet")]
-dat[, prev_wl := shift(wl_initial_cm, 1)]
-dat[, prev2_wl := shift(wl_initial_cm, 2)]
-dat[, prev_ds := shift(Ds_cm, 1)]
-dat[, prev2_ds := shift(Ds_cm, 2)]
+val <- 
+  tar_read(water_budget)[site == "152" & sample_year == 2012]
 
-big_lm <- 
-  lm(wl_initial_cm ~ 0 + offset(prev_wl) + best_precip_cm + prev_precip + prev2_precip + prev3_precip + pet_cm + prev_pet, data = dat)
+vWL <- 
+  val[!is.na(wl_initial_cm), wl_initial_cm]
+vL1P <- 
+  val[, .(wl_initial_cm, p = shift(best_precip_cm, 1))][!is.na(wl_initial_cm) & !is.na(p), p]
+vL1PET <- 
+  val[, .(wl_initial_cm, p = shift(pet_cm, 1))][!is.na(wl_initial_cm) & !is.na(p), p]
 
-dat[, wl_lm := predict(big_lm, newdata = .SD)]
+vHAT <- 
+  rep(NA, length(vWL))
 
+vHAT[1] <- 
+  vWL[1]
 
-plot(wl_lm ~ doy, data = dat[!is.na(wl_initial_cm)], type = 'l')
-lines(wl_initial_cm ~ doy, data = dat[!is.na(wl_initial_cm)], col = "blue")
+for(i in 2:length(vWL)){
+  vHAT[i] <- 
+    vHAT[i-1] + 
+    p_theta0 * (p_phi1 * vL1P[i] + p_phi2 * vL1P[i-1]) + 
+    pet_theta0 * vL1PET[i] +
+    # pmin(0, flow_mod(vHAT[i-1]))
+    # NF <- resid(mod)
+    # NF_fil <- filter(NF, rep(1/3, 3), sides = 2)
+    # df <- data.table(NF_fil, WL)
+    # mcp(list(NF_fil ~ WL, ~0 + WL + I(WL^2)), data = df)
+    ifelse(vHAT[i-1] <= 10.8,
+           0, #-(-0.199 + 0.037 * vHAT[i-1]), # Doing the opposite below the change point. Need to set change point = 0 for model?
+           -0.199 + 0.037 * (10.8) +
+             -0.0005 * (vHAT[i-1] - 10.8) +
+             -0.018 * (vHAT[i-1] - 10.8)^2)
+}
 
-ccf(dat[!is.na(wl_initial_cm), wl_initial_cm],
-    dat[!is.na(wl_initial_cm), best_precip_cm])
+vHAT_base <-
+   vWL[1] + cumsum((4.3764 * filter(vL1P, c(0.7288, 0.1691), "con", sides = 1) + -0.7821 * vL1PET)[-c(1:2)])
 
-ccf(dat[!is.na(wl_initial_cm), wl_initial_cm],
-    dat[!is.na(wl_initial_cm), pet_cm])
+plot(vHAT,
+     type = 'l',
+     ylim = c(min(c(vHAT, vWL), na.rm = TRUE), 
+              max(c(vHAT, vWL), na.rm = TRUE)))
+lines(vWL, col = "#0072B2")
 
 mod_ts <- 
-  arimax(dat[!is.na(wl_initial_cm), wl_initial_cm], 
-         order = c(1,1,0)
+  arimax(dat$Ds_cm, 
+         order = c(1,0,0)
         , xtransf = dat[!is.na(wl_initial_cm),
                         .(best_precip_cm)],
         transfer=list(c(0,0)))
@@ -151,9 +299,8 @@ t4  0
 mean P = 0.2664371
  
 mod_ds <- 
-  arimax(dat[!is.na(Ds_cm), Ds_cm], 
+  arimax(dat$Ds_cm, 
          order = c(1,0,0), 
-         include.mean = FALSE,
          xtransf = dat[!is.na(Ds_cm),
                        best_precip_cm],
          transfer = list(c(1,0)))
@@ -211,3 +358,137 @@ regress_fit <-
 by_hand_fits <- regress_fit + 
 
 plot(by_hand_fits ~ fitted(mod_ts)); abline(0, 1)
+
+
+
+
+
+
+
+min(which(!is.na(dat$wl_initial_cm)))
+
+wl_arima <- rep(NA, nrow(dat))
+wl_arima[35] <- dat[35, wl_initial_cm]
+
+predict(mod_ts,
+        n.ahead = 1, 
+        newxreg = as.matrix(dat[, .(prev_precip, prev2_precip, prev3_precip, prev_pet, prev2_pet, prev3_pet)]))
+
+for(i in 36:nrow(dat)){
+  wl_arima <- 
+    
+}
+
+df <- 
+  dat[!is.na(wl_initial_cm)]
+
+df[1:100, dataset := "training"]
+df[101:.N, dataset := "testing"]
+
+# Should be ARIMA(1,1,0) when using lag 1:3 precip and lag 1 pet
+mod <- 
+  df[dataset == "training", 
+     auto.arima(wl_initial_cm, 
+                xreg = as.matrix(.SD[, 
+                                     .(prev_precip, prev2_precip, prev3_precip, prev_pet, prev2_pet)]))]
+
+
+df[dataset == "training",
+   wl_mod := fitted(mod)]
+
+df[dataset == "testing",
+   wl_mod := predict(mod, n.ahead = .N-100, 
+                     newxreg = as.matrix(.SD[, 
+                                             .(prev_precip, prev2_precip, prev3_precip, prev_pet)]))$pred]
+
+df[, wl_mod := as.numeric(wl_mod)]
+
+plot(wl_initial_cm ~ doy,
+     data = df,
+     col = "gray50",
+     type = 'l')
+lines(wl_mod ~ doy,
+      data = df[dataset == "training"],
+      type = 'l')
+lines(wl_mod ~ doy,
+      data = df[dataset == "testing"],
+      lty = 2,
+      col = "red")
+
+
+
+dat[!is.na(wl_initial_cm),
+    wl_fitted := as.numeric(fitted(simple_ts))]
+
+simple_coefs <- 
+  coef(simple_ts)
+
+vct_fitted <- 
+  rep(NA, nrow(dat))
+
+eta <- 
+  rep(NA, nrow(dat))
+
+first_wl <- 
+  min(which(!is.na(dat$wl_initial_cm)))
+
+vct_fitted[first_wl] <- 
+  dat[first_wl, wl_initial_cm]
+
+eta[first_wl] <- 
+  vct_fitted[first_wl] - simple_coefs["prev_precip"] * dat[first_wl, prev_precip]
+
+
+
+  
+
+# vct_fitted[first_wl + 2] <- 
+#   dat[first_wl + 1, wl_initial_cm] + 
+#   simple_coefs["ar1"] * 
+#   ((diff(dat[first_wl + 0:1, wl_initial_cm])) - simple_coefs["prev_precip"] * dat[first_wl + 1, prev_precip]) + 
+#   simple_coefs["prev_precip"] * dat[first_wl + 2, prev_precip]
+
+for(i in (first_wl + 1):nrow(dat)){
+  vct_fitted[i] <- 
+    simple_coefs["ar1"] * (dat[i-1, wl_initial_cm]) # - eta[i-1])
+  
+  # eta_i <- 
+  #   vct_fitted[i] - simple_coefs["prev_precip"] * dat[i, prev_precip]
+    
+}
+
+
+
+# shift(wl_initial_cm, 1) - 
+#   # AR1 impact of differenced time series
+#   filter({(shift(wl_initial_cm, 2, 0) - shift(wl_initial_cm, 1, 0))},
+#          coef(mod_ts)[["ar1"]],
+#          "con",
+#          sides = 1) -
+#   # Sign flips for the points where p_occ and pp_occ are both true. Has to 
+#   # do with 2 negative signs
+  # coef(mod_ts)[["best_precip_cm-MA0"]] * (filter(best_precip_cm, c(-1, 0.1645), "con", sides = 1) - filter(shift(best_precip_cm, 1, 0), c(-1, 0.1645), "con", sides = 1))]
+
+
+dat[, wl_calc := vct_fitted]
+
+plot(wl_initial_cm ~ doy,
+     data = dat,
+     col = "red",
+     lty = 3,
+     type = 'l')
+lines(wl_fitted ~ doy,
+     data = dat,
+     type = 'l')
+lines(wl_calc ~ doy,
+      data = dat,
+      lty = 2,
+      col = "blue")
+
+plot(wl_fitted ~ wl_initial_cm,
+     data = dat)
+points(wl_calc ~ wl_initial_cm,
+       data = dat,
+       pch = 20,
+       col = "blue")
+
