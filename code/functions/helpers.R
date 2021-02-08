53aa3c7e5f82a4b8eb46162e3c8b7fa7c3fa7e74
Joe Shannon <jpshanno@mtu.edu>
2021-02-07 21:32:22 -0500

    Created flexible water year calculation functions

--- code/functions/helpers.R
+++ code/functions/helpers.R
@@ -217,10 +217,10 @@ calculate_climate_season <-
 ##' @author Joe Shannon
 ##' @export
 as.water_year <- 
-  function(x){
+  function(x, wy.month){
     stopifnot(any(grepl("(Date|POSIXt)", class(x))))
     
-    ifelse(month(x) > 9,
+    ifelse(month(x) > (wy.month - 1),
            year(x) + 1,
            year(x))
   }
@@ -235,9 +235,173 @@ as.water_year <-
 ##' @author Joe Shannon
 ##' @export
 as.dowy <- 
-  function(x){
+  function(x, wy.month){
     stopifnot(any(grepl("(Date|POSIXt)", class(x))))
     
-    1 + as.numeric(x - as.Date(paste(as.water_year(x)-1, "10-01", sep = "-")))
+    1 + as.numeric(x - as.Date(paste(as.water_year(x, wy.month)-1, wy.month, "01", sep = "-")))
     
+  }
+
+lyne_hollick <- 
+  function(q,
+           alpha = 0.925,
+           reflection = 0.5,
+           passes = 3,
+           na.rm = TRUE){
+    stopifnot(is.numeric(q),
+              is.numeric(alpha),
+              is.numeric(reflection),
+              is.numeric(passes))
+    
+    if(passes %% 2 != 1){
+      stop("passes must be an odd integer.")
+    }
+    
+    leading_nas <- trailing_nas <- min_q <- 0
+    
+    n_q <- 
+      length(q)
+    
+    if(any(is.na(q))){
+      
+      if(!na.rm){
+        stop("There are NA's in 'q', please use na.rm = TRUE to proceed.")
+      }
+      
+      if(sum(!is.na(q)) == 0){return(q)}
+      
+      q <- 
+        zoo::na.approx(q, na.rm = FALSE)
+      
+      if(is.na(q[1])){
+        leading_nas <- 
+          min(which(!is.na(q))) - 1
+      }
+      
+      if(is.na(q[n_q])){
+        trailing_nas <- 
+          n_q - max(which(!is.na(q)))
+      }
+      
+      q <- 
+        q[(1 + leading_nas):(length(q) - trailing_nas)]
+      
+    }
+    
+    if(any(q < 0)){
+      
+      min_q <- 
+        min(q, na.rm = TRUE)
+      
+      q <- 
+        q - min_q
+    }
+    
+    if(reflection != 0){
+      reflection_n <- 
+        floor(reflection*length(q))
+      
+      q_pad <- 
+        c(rev(q[1:reflection_n]), q, rev(q)[1:reflection_n])
+    } else {
+      q_pad <- 
+        q
+    }
+    
+    q_f <- 
+      rep(NA_real_, length(q_pad))
+    
+    q_f[1] <-
+      q_pad[1+reflection_n]
+    
+    q_b <- 
+      q_pad - q_f
+    
+    pass <- 0
+    
+    q_prev <- 
+      q_pad
+    
+    while(pass < passes){
+      for (i in 2:length(q_pad)){
+        
+        q_f[i] <-
+          # lyne_hollick_eq(alpha, q_f, q_prev, i)
+          alpha * q_f[i -1] + ((1 + alpha) / 2) * (q_prev[i] - q_prev[i - 1])
+        
+        if(q_f[i] < 0) q_f[i] <- 0
+        if(q_f[i] > q_prev[i]) q_f[i] <- q_prev[i]
+        
+        q_b[i] <-
+          q_prev[i] - q_f[i]
+      }
+      
+      pass <- 
+        pass + 1
+      
+      if(pass < passes){
+        
+        q_prev <- 
+          rev(q_b)
+        
+        q_f[1] <-
+          rev(q_b)[1]
+        
+        q_b[1] <- 
+          q_prev[1] - q_f[1]
+        
+      }
+      
+    }
+    c(rep(NA_real_, leading_nas), 
+      q_b[(reflection_n + 1):(length(q_b) - reflection_n)] + min_q,
+      rep(NA_real_, trailing_nas))
+  }
+
+
+ytd_sum <- 
+  function(x){
+    
+    stopifnot(is.numeric(x))
+    
+    if(all(!is.na(x))){return(cumsum(x))}
+    
+    indx <- 
+      which(!is.na(x))
+    
+    # x <- 
+    #   zoo::na.approx(x, rule = 2)
+    
+    ytd_x <- 
+      cumsum(na.omit(x))
+    
+    out <- 
+      rep(NA, times = length(x))
+    
+    out[indx] <- 
+      ytd_x
+    
+    out
+  }
+
+diff_na <- 
+  function(x){
+    
+    stopifnot(is.numeric(x))
+    
+    if(all(!is.na(x))){return(c(NA, diff(x)))}
+    
+    indx <- 
+      which(!is.na(x))
+    
+    dx <- 
+      c(NA, diff(na.omit(x)))
+    
+    out <- 
+      rep(NA, length(x))
+    
+    out[indx] <- 
+      dx
+    
+    out
   }
