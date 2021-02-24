083d662d70f0cebc0a29532175aec3d91d025394
Joe Shannon <jpshanno@mtu.edu>
2021-02-24 12:01:30 -0500

    Merge additional CM outputs into water budget data

--- code/functions/merge_external_met.R
+++ code/functions/merge_external_met.R
@@ -26,6 +26,10 @@ merge_external_met <- function(data, external.met) {
                    "tmin_c",
                    "tmax_c",
                    "melt_cm",
+                   "rain_cm",
+                   "snow_cm",
+                   "swe_cm",
+                   "total_input_cm",
                    "water_availability_cm",
                    "ytd_water_availability_cm") := 
                  map2_dfr(site, sample_date, 
@@ -40,19 +44,31 @@ merge_external_met <- function(data, external.met) {
                                        tmin_c = weighted.mean(tmin_c, weight),
                                        tmax_c = weighted.mean(tmax_c, weight),
                                        melt_cm = weighted.mean(melt_cm, weight),
-                                       water_availability_cm = weighted.mean(water_availability_cm, weight),
-                                       ytd_water_availability_cm = weighted.mean(ytd_water_availability_cm, weight))]
+                                       rain_cm = weighted.mean(rain_cm, weight),
+                                       snow_cm = weighted.mean(snow_cm, weight),
+                                       swe_cm = weighted.mean(swe_cm, weight),
+                                       total_input_cm = weighted.mean(total_input_cm, weight),
+                                       water_availability_cm = weighted.mean(water_availability_cm, weight, na.rm = TRUE),
+                                       ytd_water_availability_cm = weighted.mean(ytd_water_availability_cm, weight, na.rm = TRUE))]
                           })
   ]
   
   external.met[, `:=`(l1_pet_cm = shift(pet_cm, 1),
                       l1_melt_cm = shift(melt_cm, 1),
+                      l1_rain_cm = shift(rain_cm, 1),
+                      l1_snow_cm = shift(snow_cm, 1),
+                      l1_swe_cm = shift(swe_cm, 1),
+                      l1_total_input_cm = shift(total_input_cm, 1),
                       l1_water_availability_cm = shift(water_availability_cm, 1),
                       l1_ytd_water_availability_cm = shift(ytd_water_availability_cm, 1)),
                by = .(station_name)]
   
   water_budget[, c("l1_pet_cm", 
                    "l1_melt_cm",
+                   "l1_rain_cm",
+                   "l1_snow_cm",
+                   "l1_swe_cm",
+                   "l1_total_input_cm",
                    "l1_water_availability_cm",
                    "l1_ytd_water_availability_cm") := 
                  map2_dfr(site, sample_date, 
@@ -65,6 +81,10 @@ merge_external_met <- function(data, external.met) {
                             
                             ex_dat[, .(l1_pet_cm = weighted.mean(l1_pet_cm, weight, na.rm = TRUE),
                                        l1_melt_cm = weighted.mean(l1_melt_cm, weight, na.rm = TRUE),
+                                       l1_rain_cm = weighted.mean(l1_rain_cm, weight, na.rm = TRUE),
+                                       l1_snow_cm = weighted.mean(l1_snow_cm, weight, na.rm = TRUE),
+                                       l1_swe_cm = weighted.mean(l1_swe_cm, weight, na.rm = TRUE),
+                                       l1_total_input_cm = weighted.mean(l1_total_input_cm, weight, na.rm = TRUE),
                                        l1_water_availability_cm = weighted.mean(l1_water_availability_cm, weight, na.rm = TRUE),
                                        l1_ytd_water_availability_cm = weighted.mean(l1_ytd_water_availability_cm, weight, na.rm = TRUE))]
                           })
