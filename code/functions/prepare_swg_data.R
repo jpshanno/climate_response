15f9cb005e7d9efd4f65ade36045c8baf4044f77
Joe Shannon <jpshanno@mtu.edu>
2021-03-23 11:32:02 -0400

    Filled missing data using IDW from all downloaded GHCN-D data. Missing data is evaluated and sites selected before backfilling

--- code/functions/prepare_swg_data.R
+++ code/functions/prepare_swg_data.R
@@ -8,7 +8,7 @@
 ##' @return
 ##' @author Joe Shannon
 ##' @export
-prepare_swg_data <- 
+prepare_ghcnd_swg_data <- 
   function(ncei.paths, 
            ghcnd.units,
            additional.stations = NULL,
@@ -99,108 +99,36 @@ prepare_swg_data <-
                                         tmax_c <= 0.75 * days_in_month)),
                    by = .(station_name)][n_months > 336, .(station_name)]
     
+    # Use IDW to fill-in missing data (do it prior to removing unused stations
+    # because they still have useful data for the IDW backfilling)
+    
+    station_weights <- 
+      calculate_weights(ghcnd_data)
+    
+    idw_data <- 
+      map_dfr(colnames(station_weights), 
+              ~ghcnd_data[station_name != .x, 
+                          c(station_name = .x,
+                            map(.SD, 
+                                function(X){
+                                  weighted.mean(x = X, 
+                                                w = station_weights[.x, station_name],
+                                                na.rm = TRUE)})), 
+                          by = .(sample_date), 
+                          .SDcols = c("tmin_c", "tmax_c", "precip_cm")])
+    
     ghcnd_data <- 
       ghcnd_data[stations_to_use,
                  on = "station_name"]
   
-    format_swg_data(ghcnd_data)
+    ghcnd_data <- 
+      ghcnd_data[idw_data,
+                 on = c("station_name", "sample_date"),
+                 `:=`(tmin_c = fcoalesce(tmin_c, i.tmin_c),
+                      tmax_c = fcoalesce(tmax_c, i.tmax_c),
+                      precip_cm = fcoalesce(precip_cm, i.precip_cm))]
+    
+    swg_format_data(ghcnd_data)
       
   }
 
-format_swg_data <- 
-  function(data){
-    
-    dat_names <- 
-      names(data)
-    
-    stopifnot("station_name" %in% dat_names,
-              "sample_date" %in% dat_names,
-              "precip_cm" %in% dat_names,
-              "tmax_c" %in% dat_names,
-              "tmin_c" %in% dat_names)
-    
-    data <- 
-      copy(data)
-  
-    data[, `:=`(sample_month = month(sample_date),
-                sample_year = year(sample_date),
-                sample_season = as.climate_season(sample_date, TRUE))]
-    
-    # Use NA_real_ not 0 because the precip amount model only deals with the
-    # amount, so the relationship between the covariates and precip amount would
-    # get muddled if we included 0s
-    data[, `:=`(precip_occur = as.numeric(precip_cm > 0.01),
-                precip_amount = ifelse(precip_cm < 0.01, NA_real_, precip_cm))]
-    
-    data[, `:=`(tmin_l1 = shift(tmin_c, 1),
-                tmax_l1 = shift(tmax_c, 1),
-                precip_occur_l1 = shift(precip_occur, 1)),
-         by = .(station_name)]
-    
-    # After lagging variables remove 1979-12-31 to avoid messing up trend &
-    # harmonics
-    data <- 
-      data[sample_date != as.Date("1979-12-31")]
-    
-    data[, temperature_trend := seq(-1, 1, length.out = .N),
-         by = .(station_name)]
-    
-    data[, `:=`(harmonic_cos = cos((2*pi*(1:.N / .N))),
-                harmonic_sin = sin((2*pi*(1:.N / .N)))),
-         by = .(station_name, sample_year)]
-    
-    
-    # Site & Regional Monthly & Seasonal Temperatures
-    data[, `:=`(site_monthly_tmin_c = mean_na(tmin_c), 
-                site_monthly_tmax_c = mean_na(tmax_c)),
-         by = .(station_name, sample_year, sample_month)]
-
-    data[, `:=`(regional_monthly_tmin_c = mean_na(site_monthly_tmin_c),
-                regional_monthly_tmax_c = mean_na(site_monthly_tmax_c)),
-         by = .(sample_year, sample_month)]
-    
-    data[, `:=`(regional_seasonal_tmin_c = mean_na(regional_monthly_tmin_c),
-                regional_seasonal_tmax_c = mean_na(regional_monthly_tmax_c)),
-         by = .(sample_year, sample_season)]
-    
-    # Precip
-    # sum precip by site, year, month
-    data[, site_total_monthly_precip_cm := sum_na(precip_cm),
-         by = .(station_name, sample_year, sample_month)]
-
-    # mean of summed precip by year, month
-    data[, regional_mean_monthly_precip_cm := mean_na(site_total_monthly_precip_cm),
-         by = .(sample_year, sample_month)]
-    
-    # sum of mean_monthly_precip by year, season
-    # Needs to be divided by .N because regional_mean_monthly_precip_cm is repeated across sites
-    data[, regional_total_seasonal_precip_cm := sum_na(regional_mean_monthly_precip_cm) / .N,
-         by = .(sample_year, sample_season)]
-    
-   
-    dcast(data[, 
-               .(station_name,
-                 lat,
-                 lon,
-                 elevation_m,
-                 sample_date, 
-                 sample_month,
-                 sample_season, 
-                 tmin_c,
-                 tmax_c,
-                 tmin_l1,
-                 tmax_l1,
-                 harmonic_cos, 
-                 harmonic_sin, 
-                 precip_occur,
-                 precip_occur_l1,
-                 precip_amount,
-                 temperature_trend,
-                 regional_precip_cm = regional_total_seasonal_precip_cm,
-                 regional_tmin_c = regional_seasonal_tmin_c,
-                 regional_tmax_c = regional_seasonal_tmax_c)], 
-          ... ~ sample_season,
-          value.var = c("regional_tmin_c", "regional_tmax_c", "regional_precip_cm"), 
-          fill = 0)
-
-  }
