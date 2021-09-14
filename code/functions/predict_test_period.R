6c473b3abb5a06e4a916e2428184bea6f7b329d7
Joe Shannon <jpshanno@mtu.edu>
2021-09-13 23:18:28 -0400

    Function to predict train period

--- code/functions/predict_test_period.R
+++ code/functions/predict_test_period.R
@@ -19,3 +19,21 @@ predict_test_period <- function(data, model.params) {
        by = .(site, water_year)][]
 
 }
+
+
+predict_train_period <- function(data, model.params) {
+  
+  lapply(data,
+    function(x) {
+      x[, 
+           cbind(sample_date,
+                 wl_initial_cm,
+                 site_status = site_status[1],
+                 wetland_model(data = .SD,
+                               params = model.params[CJ(.BY[[1]], .SD$site_status[1]), params[[1]]])),
+           by = .(site, water_year)][]
+    }
+  )
+  
+  
+}
