285016f466020ca53042b420b66317e4c08d90ef
Joe Shannon <joe.shannon@ncx.com>
2021-10-04 06:46:41 -0400

    Run predict on future forested conditions

--- code/functions/predict_test_period.R
+++ code/functions/predict_test_period.R
@@ -21,6 +21,18 @@ predict_test_period <- function(data, model.params) {
 }
 
 
+predict_test_period_future_forest <- function(data, model.params) {
+  
+  data[, 
+       cbind(sample_date,
+             wl_initial_cm,
+             site_status = "Future Forested",
+             wetland_model(data = .SD,
+                           params = model.params[CJ(.BY[[1]], "Future Forested"), params[[1]]])),
+       by = .(site, water_year)][]
+  
+}
+
 predict_train_period <- function(data, model.params) {
   
   lapply(data,
