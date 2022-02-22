78201f53b1f07d33c5fa4b52db13296d255a0c1a
Joe Shannon <joe.shannon@ncx.com>
2022-02-21 22:25:04 -0500

    Working stan model and sampling

---
+++ code/scratch/stan_model.R
@@ -0,0 +1,66 @@
+####### WORK WITH ONLY TREATED SITES TO AVOID UNEVEN GROUP SIZES
+
+### Likelihood should probably go back to dnorm with obs sd
+### Need to think about matching scale of ll for likelihood and prior
+### Need to get proposal function better tuned
+
+source("code/load_project.R")
+tar_load(training_data)
+tar_load(testing_data)
+tar_load(treatment_sites)
+tar_load(esy_functions)
+esy_functions[, min_esy := pred_fun[[1]](max_wl, -9999), by = "site"]
+training_data[["control"]][esy_functions, `:=`(max_wl = i.max_wl, funESY = i.pred_fun, minESY = i.min_esy)]
+con_dat <- training_data[["control"]][site %in% treatment_sites]
+con_dat <- con_dat[(!(month(sample_date) == 2 & mday(sample_date) == 29)),]
+
+site <- "151"
+dat <- con_dat[site == "151"]
+
+init_weight <- 1 / 365
+
+# Weights increase asymmetrically as water levels drop
+wghts <- pmax(init_weight, init_weight * (dat$max_wl - dat$wl_initial_cm))
+
+# Weights are squared
+wghts <- wghts^2
+wghts[is.na(dat$wl_initial)] <- 0
+
+stan_data <- list(
+  D = 365L,
+  K = 1L,
+  pet = matrix(dat$pet_cm, nrow = 1),
+  rain = matrix(dat$rain_cm, nrow = 1),
+  melt = matrix(dat$melt_cm, nrow = 1),
+  wghts = matrix(wghts, nrow = 1),
+  maxWL = as.array(unique(dat$max_wl)), # wrapping in as.array found as recommended fix for dims 'declared=(1); dims found=()' error
+  y = matrix(nafill(dat$wl_initial_cm, "const", -9999), nrow = 1),
+  esyParams = matrix(c(1.0764391, 9.56079966326987, 1.17368355521875, 0.00762101076778987), nrow = 1),
+  ySD = as.array(sd(diff(dat$wl_initial_cm), na.rm = TRUE))
+)
+
+
+library(cmdstanr)
+library(posterior)
+library(bayesplot)
+mod <- cmdstan_model(stan_file = "code/scratch/wetland_model.stan")
+# Last fit was with all 4 parameters. Looks like 1 chain went bad when bQ was
+# included. Probably have to fine tune how it gets used
+fit <- mod$sample(
+  data = stan_data,
+  seed = 1234567,
+  chains = 4,
+  parallel_chains = 4,
+  refresh = 500,
+  adapt_delta = 0.9,
+  max_treedepth = 10
+)
+
+fit$summary()
+mcmc_hist(fit$draws(c("bPET", "bRain", "bMelt", "bQ")))
+
+fit_mcmc <- as_mcmc.list(fit)
+
+# color_scheme_set("mix-blue-pink")
+mcmc_trace(fit_mcmc,  pars = c("bPET", "bRain", "bMelt", "bQ"), n_warmup = 1000,
+                facet_args = list(nrow = 2, labeller = label_parsed))
