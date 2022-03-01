717a1432cf74cf0e8cbea7716432717f78e507e7
Joe Shannon <joe.shannon@ncx.com>
2022-02-28 23:16:29 -0600

    R updates

--- code/scratch/stan_model.R
+++ code/scratch/stan_model.R
@@ -14,30 +14,37 @@ training_data[["control"]][esy_functions, `:=`(max_wl = i.max_wl, funESY = i.pre
 con_dat <- training_data[["control"]][site %in% treatment_sites]
 con_dat <- con_dat[(!(month(sample_date) == 2 & mday(sample_date) == 29)),]
 
-site <- "151"
-dat <- con_dat[site == "151"]
+library(cmdstanr)
+library(posterior)
+library(bayesplot)
 
 # Weights increase asymmetrically as water levels drop
 create_weights <- function(x, scale) {
-  init_weight <- 1 / sum(!is.na(x))
+  init_weight <- 1 / 365 # sum(!is.na(x))
   wghts <- pmax(init_weight, init_weight * (scale - x))
   # Weights are squared
   wghts <- wghts^2
   wghts[is.na(x)] <- 0
   wghts <- wghts / sum(wghts)
+  assertthat::assert_that(assertthat::noNA(wghts))
+  wghts
 }
 
 create_data_matrix <- function(data, var) {
-  lapply(
+  res <- lapply(
     X = sort(unique(data[["site"]])),
     FUN = \(x) matrix(data[site == x][[var]], nrow = 1)
   ) %>%
   purrr::reduce(rbind)
+
+  assertthat::assert_that(assertthat::noNA(res))
+  res
 }
 
 con_dat[, wghts := create_weights(wl_initial_cm, max_wl), by = .(site)]
 con_dat[, filled_wl := nafill(wl_initial_cm ,"const", -9999)]
-esy_functions[sort(unique(con_dat$site))]$pred_fun
+# esy_functions[sort(unique(con_dat$site))]$pred_fun
+# esy_functions[sort(unique(con_dat$site))]$min_esy
 esy_params <- matrix(
   c(
     0.9440797, 9.68796681237216, 0.963832093292945, 0.011490387703557,
@@ -67,32 +74,73 @@ stan_data <- list(
   ySD = obs_sds
 )
 
+init_values <- function() {
+  x <- list(
+    bPET = runif(1, 0.9, 1.1),
+    bRain = runif(1, 0.9, 1.1),
+    bMelt = runif(1, 0.9, 1.1),
+    bQ = runif(1, 0.4, 0.7)
+  )
+  x$sigma <- rep(4, 8)
+  x
+}
 
-library(cmdstanr)
-library(posterior)
-library(bayesplot)
-mod <- cmdstan_model(stan_file = "code/scratch/wetland_model.stan")
-# On the last fit with all 4 parameters it look like 1 chain went bad when bQ
-# was included. Probably have to fine tune the priors or the specification in
-# the model
+
+mod <- cmdstan_model(
+  stan_file = "code/scratch/wetland_model.stan",
+  include_paths = file.path(getwd(), "code/scratch/"),
+  force_recompile = TRUE
+  )
 fit <- mod$sample(
   data = stan_data,
   seed = 1234567,
   chains = 4,
   parallel_chains = 4,
-  refresh = 500,
-  adapt_delta = 0.9,
-  max_treedepth = 10
+  adapt_delta = 0.95,
+  max_treedepth = 11,
+  iter_warmup = 100,
+  iter_sampling = 100,
+  refresh = 50,
+  save_warmup = TRUE,
+  init = init_values
 )
 
 fit$summary()
-mcmc_hist(fit$draws(c("bPET", "bRain", "bMelt", "bQ")))
+mcmc_hist(fit$draws(c("bPET", "bRain", "bMelt", "bQ"), inc_warmup = TRUE))
 
 fit_mcmc <- as_mcmc.list(fit)
 
 # color_scheme_set("mix-blue-pink")
-mcmc_trace(fit_mcmc,  pars = c("bPET", "bRain", "bMelt", "bQ"), n_warmup = 1000,
+mcmc_trace(fit_mcmc,  pars = c("bPET", "bRain", "bMelt", "bQ"), n_warmup = 100,
                 facet_args = list(nrow = 2, labeller = label_parsed))
+
+# Hierarchical Model
+mod_hc <- cmdstan_model(
+  stan_file = "code/scratch/wetland_model_hierarchical.stan",
+  include_paths = file.path(getwd(), "code/scratch/"),
+  force_recompile = TRUE
+  )
+fit_hc <-  mod_hc$sample(
+  data = stan_data,
+  seed = 1234567,
+  chains = 4,
+  parallel_chains = 4,
+  adapt_delta = 0.9,
+  max_treedepth = 10,
+  iter_warmup = 100,
+  iter_sampling = 100,
+  refresh = 50
+)
+
+fit_hc$summary()
+mcmc_hist(fit_hc$draws(c("bPET", "bRain", "bMelt", "bQ")))
+
+fit_mcmc_hc <- as_mcmc.list(fit_hc)
+
+# color_scheme_set("mix-blue-pink")
+mcmc_trace(fit_mcmc_hc,  pars = c("bPETPop", "bRainPop", "bMeltPop", "bQPop"), n_warmup = 1000,
+                facet_args = list(nrow = 2, labeller = label_parsed))
+
 
 
 new_params <- list(
