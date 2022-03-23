961fc20f41e6bb60ba6c5001c5597826ca1d5709
Joe Shannon <joe.shannon@ncx.com>
2022-03-22 23:01:48 -0400

    Adapt delta 0.99

--- code/scratch/stan_model.R
+++ code/scratch/stan_model.R
@@ -127,7 +127,7 @@ fit <- mod$sample(
   seed = 1234567,
   chains = 4,
   parallel_chains = 4,
-  adapt_delta = 0.95,
+  adapt_delta = 0.99,
   # max_treedepth = 11,
   iter_warmup = 1000,
   iter_sampling = 1000,
