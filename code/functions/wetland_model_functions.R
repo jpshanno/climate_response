f98432b46f803de53cbb37ea5f3b14f2f76a6356
Joe Shannon <joe.shannon@ncx.com>
2022-04-22 00:31:53 -0400

    Esy, model fit, and parameter prep functions

--- code/functions/wetland_model_functions.R
+++ code/functions/wetland_model_functions.R
@@ -67,7 +67,7 @@ numeric_mode <-
 ##' @author Joe Shannon
 ##' @export
 ##' 
-build_esy_functions <- 
+fit_esy_model <- 
   function(data){
     
     drawdown <- 
@@ -77,6 +77,12 @@ build_esy_functions <-
     
     drawdown[, ytd_water_balance := cumsum(rain_cm + pet_cm + melt_cm),
              by = .(site, water_year)]
+
+    # 053 is a different year, so it doesn't match the other sites (it has more
+    # snowmelt because of a later spring in 2013 than 2012). Setting 053 to 
+    # have a maximum of 0 like all the other sites
+    drawdown[site == "053", ytd_water_balance := ytd_water_balance - max(ytd_water_balance)]
+
     # New potential Esy model:
     # TODO: figure out why 053 water balance is so different from the other
     # sites. The shape is right, but there's a huge shift into positive water
@@ -97,79 +103,226 @@ build_esy_functions <-
     # parameter alpha that ranges from 0-1 around some inflection point.
     # drawdown ~ alpha * high_esy + (1 - alpha) * low_esy
     # emperical_esy = deriv(drawdown_mod)
-    brm
+
     drawdown[, 
-             c("drawdown_emp", "esy_emp",
-               "esy_x_intercept") := {
-                 
-                 mod <- 
-                   robustbase::nlrob(wl_initial_cm ~ quad(ytd_water_balance, 
-                                                          b0 = b0, b1 = b1, b2 = b2),
-                                     data = .SD, 
-                                     na.action = na.exclude,
-                                     maxit = 50,
-                                     start = list(b0 = 8, b1 = 1, b2 = -1))
-                 
-                 list(drawdown_emp = predict(mod, newdata = .SD),
-                      esy_emp = quad_prime(mod = mod, wa = .SD[, ytd_water_balance]),
-                      esy_x_intercept = quad_x_intercept(mod))
-                 
-               },
-             by = .(site)]
+      c("drawdown_emp", "esy_emp",
+        "esy_x_intercept") := {
+
+          mod <-
+            robustbase::nlrob(wl_initial_cm ~ quad(ytd_water_balance, 
+                                                  b0 = b0, b1 = b1, b2 = b2),
+                              data = .SD, 
+                              na.action = na.exclude,
+                              maxit = 50,
+                              start = list(b0 = 8, b1 = 1, b2 = -1))
+
+          list(drawdown_emp = predict(mod, newdata = .SD),
+              esy_emp = quad_prime(mod = mod, wa = .SD[, ytd_water_balance]),
+              esy_x_intercept = quad_x_intercept(mod))
+
+        },
+      by = .(site)]
     # split(drawdown, f = drawdown$site) %>% purrr::map_dfr(~coef(lm((34.77 - 12.894) * (exp(-0.059 * ytd_water_balance) * 0.059) ~ wl_initial_cm, data = .x)), .id = "site") 
-    esy_form <- 
+    esy_form <-
       bf(esy_emp ~ a - (a - b) * exp (c * wl_initial_cm),
          a + b + c ~ 0 + site,
          nl = TRUE)
-    
-    esy_priors <- 
+
+    esy_priors <-
       prior(nlpar = "a", normal(10, 0.5), lb = 0) +
       prior(nlpar = "b", normal(2, 0.25), lb = 0) +
       prior(nlpar = "c", normal(0.01, 0.002), lb = 0) +
       prior(gamma(2, .1), class = nu)
-    
-    brm_esy <- 
+    # ggplot(drawdown) + aes(x = wl_initial_cm, y = esy_emp, color = site) + geom_point() + geom_function(fun = ~9.693928 - (9.693928 - 1.036578) * exp (0.01578 * .x), color = "black")
+    brm_esy <-
       brm(esy_form,
           prior = esy_priors,
           family = student,
           cores = 4,
-          save_model = "output/models/esy_model.stan", 
+          save_model = "code/esy_model.stan",
           data = drawdown[!is.na(wl_initial_cm)])
-    
-    esy_coefs <- 
-      fixef(brm_esy)
-    
-    esy_a <- 
+
+    brm_esy
+}
+
+get_esy_coefs <- function(esy_mod){
+    esy_coefs <-
+      fixef(esy_mod)
+
+    esy_a <-
       esy_coefs[str_detect(rownames(esy_coefs), "a_"), "Estimate"] %>% 
       set_names(~str_extract(., "[0-9]{3}"))
-    
-    esy_b <- 
+
+    esy_b <-
       esy_coefs[str_detect(rownames(esy_coefs), "b_"), "Estimate"] %>% 
       set_names(~str_extract(., "[0-9]{3}"))
-    
-    esy_c <- 
+
+    esy_c <-
       esy_coefs[str_detect(rownames(esy_coefs), "c_"), "Estimate"] %>% 
       set_names(~str_extract(., "[0-9]{3}"))
-    
-    drawdown[, `:=`(a = esy_a[[.BY[[1]]]],
-                    b = esy_b[[.BY[[1]]]],
-                    c = esy_c[[.BY[[1]]]]), 
-             by = .(site)]
-    
-    drawdown[, esy_hat := a - (a - b) * exp (c * wl_initial_cm)]
-    
-    esy_functions <- 
-      drawdown[, 
-               .(pred_fun = list(as.function(list(wl = NULL, 
-                                                  min.esy = NULL,
-                                                  bquote(pmax(min.esy,
-                                                              .(a) - (.(a) - .(b)) * exp (.(c) * wl)),
-                                                         where = .SD[1, .(a, b, c)])))),
-                 max_wl = numeric_mode(wl_initial_cm, na.rm = TRUE)),
-            keyby = .(site)]
-    
-    esy_functions
+
+    dat <- as.data.table(esy_mod$data)
+
+    esy_coefs <- dat[,
+      j = .(
+        a = esy_a[[.BY[[1]]]],
+        b = esy_b[[.BY[[1]]]],
+        c = esy_c[[.BY[[1]]]],
+        max_wl = numeric_mode(wl_initial_cm, na.rm = TRUE)
+      ),
+      keyby = .(site)]
+
+    esy_coefs[, min_esy := a - (a - b) * exp (c * max_wl)]
+
+    esy_coefs
+}
+
+build_esy_functions <- function(esy_coefs) {
+    # drawdown[, esy_hat := a - (a - b) * exp (c * wl_initial_cm)]
+
+  esy_coefs[,
+    j = .(
+      esy_func = list(as.function(list(wl = NULL, 
+                                      min.esy = NULL,
+                                      bquote(pmax(min.esy,
+                                                  .(a) - (.(a) - .(b)) * exp (.(c) * wl)),
+                                              where = .SD[1, .(a, b, c)])))),
+      max_wl,
+      min_esy
+    ),
+    keyby = .(site)
+  ]
+
   }
+
+# Weights increase asymmetrically as water levels drop
+  create_weights <- function(x, scale) {
+    init_weight <- 1 / !is.na(x)
+    wghts <- pmax(init_weight, init_weight * (scale - x))
+    # Weights are squared
+    wghts <- wghts^2
+    wghts[is.na(x)] <- 0
+    # wghts <- wghts / sum(wghts)
+    assertthat::assert_that(assertthat::noNA(wghts))
+    wghts
+  }
+
+  create_data_matrix <- function(data, var) {
+    res <- lapply(
+      X = sort(unique(data[["site"]])),
+      FUN = \(x) matrix(data[site == x][[var]], nrow = 1)
+    ) %>%
+    purrr::reduce(rbind)
+
+    assertthat::assert_that(assertthat::noNA(res))
+    res
+  }
+
+  create_data_array <- function(data, var) {
+    arr <- sapply(
+      X = unique(data[["site_status"]]),
+      FUN = \(x) create_data_matrix(data[site_status == x], var),
+      simplify = "array"
+    )
+    aperm(arr, c(3, 1:2))
+  }
+
+  generate_values <- function() {
+      list(
+        bPET = runif(1, 0.9, 1.1),
+        bRain = runif(1, 1.4, 1.6),
+        bTreat = runif(1, 0.45, 0.65),
+        bphiRain = runif(1, 0.1, 0.3),
+        bQ = runif(1, 0.1, 0.3),
+        sigma = array(runif(1, 0.9, 1.1)),
+
+        taubPET = runif(1, 0.01, 0.03),
+        # taubPETStatus = runif(2, 0.01, 0.03),
+        taubRain = runif(1, 0.01, 0.03),
+        tauphiRain = runif(1, 0.01, 0.03),
+
+        # bPETStatus = runif(2, 0.9, 1.1),
+
+        gPET = runif(8, 0.9, 1.1),
+        # gPETTreated = runif(8, 0.45, 0.55),
+        gTreat = runif(8, 0.45, 0.55),
+        gRain = runif(8, 0.9, 1.1),
+        gphiRain = runif(8, 0.9, 1.1)
+      )
+    }
+
+  init_values <- function(chain_id) {
+    set.seed(8675309 + chain_id)
+    generate_values()
+  }
+
+#' Fit Wetland Model
+#' 
+#' Fit the hierarchical wetland model
+fit_wetland_model <- function(data, esy_coefs, out_path) {
+
+  # Combine all data for joint fitting
+  con_dat <- rbindlist(data)
+  setkey(con_dat, "site", "sample_date")
+
+  con_dat[esy_coefs, max_wl := i.max_wl]
+  con_dat <- con_dat[(!(month(sample_date) == 2 & mday(sample_date) == 29)),]
+  con_dat[, wghts := create_weights(wl_initial_cm, max_wl), by = .(site, site_status)]
+  con_dat[, filled_wl := nafill(wl_initial_cm ,"const", -9999)]
+
+  stan_data <- list(
+    D = 365L,
+    K = length(unique(con_dat$site)),
+    T = length(unique(con_dat$site_status)),
+    pet = create_data_array(con_dat, "pet_cm"),
+    rain = create_data_array(con_dat, "rain_cm"),
+    melt = create_data_array(con_dat, "melt_cm"),
+    wghts = create_data_array(con_dat, "wghts"),
+    y = create_data_array(con_dat, "filled_wl"),
+    esyParams = as.matrix(esy_coefs[, .(min_esy, a, b, c)]),
+    maxWL = create_data_matrix(esy_coefs, "max_wl")[, 1]
+  )
+
+  mod <- cmdstan_model(
+    stan_file = "code/wetland_model.stan",
+    force_recompile = TRUE
+    )
+
+  fit <- mod$variational(
+    data = stan_data,
+    seed = 20220420,
+    iter = 250000,
+    init = init_values,
+    output_dir = "output/models",
+    output_basename = "wetland_model"
+  )
+
+  fit$save_object(out_path)
+
+  out_path
+
+}
+
+format_model_parameters <- function(site_status, in_path, esy_functions) {
+  match.arg(site_status, c("Control", "Treated"))
+  model <- readRDS(in_path)
+  params_dt <- as.data.table(model$summary())[str_detect(variable, "^g")]
+  params_dt[,
+    `:=`(
+      idx = as.numeric(str_extract(variable, "[0-9]{1}")),
+      variable = str_extract(variable, "^[A-Za-z]+")
+    )
+  ]
+  params_dt[, site := esy_functions$site[idx]]
+  wide <- dcast(params_dt, site ~ variable, value.var = "median")
+  # T = 1 for Control, 2 for Treated
+  wide[, T := 1 + (site_status == "Treated")]
+  setkey(wide, "site")
+  out <- wide[esy_functions]
+
+  out[, .(params = list(as.list(.SD))), keyby = .(site)]
+
+}
 
 
 ##' @title Wetland model
